1d10ba054a8ba1cae0a6d91b5b7da63a98c5ac60
cuteriveranimals <cuteriveranimals@gmail.com>
2023-06-27 12:04:05 +0200

    Wrap it up

--- src/AppBody.tsx
+++ src/AppBody.tsx
@@ -1,11 +1,137 @@
 import React from "react";
 import styled from "styled-components";
+import {
+  isOverviewTabContent,
+  isPensionTabContent,
+  isSavingsTabContent,
+  OverviewTabContent,
+  PensionTabContent,
+  SavingsTabContent,
+  useAppStore,
+} from "./appStore";
+import { colorOrange } from "./atoms";
+
+type OverviewTabContentProps = {
+  readonly content: OverviewTabContent;
+};
+
+type SavingsTabContentProps = {
+  readonly content: SavingsTabContent;
+};
+
+type PensionTabContentProps = {
+  readonly content: PensionTabContent;
+};
 
 const AppBodyContainer = styled.div`
   flex: 1;
+  margin: 40px 20px;
 `;
+
+const OverviewCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid #333;
+  padding: 20px;
+  gap: 10px;
+`;
+
+const CardTitle = styled.div`
+  color: ${colorOrange};
+  font-size: 1.2em;
+`;
+
+const PensionCardTitle = styled.div`
+  font-size: 1.3em;
+  margin-bottom: 20px;
+`;
+
+const SavingsCard = styled(OverviewCard)`
+  --gap: 20px;
+  background-color: #333;
+  width: calc(50% - (3 * var(--gap)));
+
+  &:first-of-type {
+    width: 100%;
+  }
+`;
+
+const SavingsCardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: space-between;
+`;
+
+const PensionCard = styled.div`
+  background-color: #333;
+  padding: 20px;
+`;
+
+const OverviewTab = ({
+  content: { title, subtitle },
+}: OverviewTabContentProps) => {
+  return (
+    <OverviewCard>
+      <CardTitle>{title}</CardTitle>
+      <div>{subtitle}</div>
+    </OverviewCard>
+  );
+};
+
+const SavingsTab = ({ content: { cards } }: SavingsTabContentProps) => {
+  return (
+    <SavingsCardsContainer>
+      {cards.map((card) => (
+        <SavingsCard key={card.title}>
+          <CardTitle>{card.title}</CardTitle>
+          <div>{card.subtitle}</div>
+        </SavingsCard>
+      ))}
+    </SavingsCardsContainer>
+  );
+};
+
+const PensionTab = ({
+  content: { title, description },
+}: PensionTabContentProps) => {
+  return (
+    <div>
+      <h2>Other pensions</h2>
+      <PensionCard>
+        <PensionCardTitle>{title}</PensionCardTitle>
+        {description}
+      </PensionCard>
+    </div>
+  );
+};
 const AppBody = () => {
-  return <AppBodyContainer>body</AppBodyContainer>;
+  const activeTabId = useAppStore(({ activeTabId }) => activeTabId);
+  const content = useAppStore(
+    ({ tabContentMap }) => tabContentMap[activeTabId].body
+  );
+  if (isOverviewTabContent(content)) {
+    return (
+      <AppBodyContainer>
+        <OverviewTab content={content} />
+      </AppBodyContainer>
+    );
+  }
+  if (isSavingsTabContent(content)) {
+    return (
+      <AppBodyContainer>
+        <SavingsTab content={content} />
+      </AppBodyContainer>
+    );
+  }
+  if (isPensionTabContent(content)) {
+    return (
+      <AppBodyContainer>
+        <PensionTab content={content} />
+      </AppBodyContainer>
+    );
+  }
+  return null;
 };
 
 export default AppBody;
--- src/appStore.ts
+++ src/appStore.ts
@@ -17,10 +17,42 @@ type TabHeaderContent = {
   readonly description: string | null;
 };
 
-// TODO
-type TabBodyContent = {
-  readonly content?: string;
+type Card = {
+  readonly title: string;
+  readonly subtitle: string;
 };
+
+export type OverviewTabContent = {
+  readonly type: TabId.OVERVIEW;
+} & Card;
+
+export type SavingsTabContent = {
+  readonly type: TabId.SAVINGS;
+  readonly cards: Card[];
+};
+
+export type PensionTabContent = {
+  readonly type: TabId.PENSION;
+  readonly title: string;
+  readonly description: string;
+};
+
+type TabBodyContent =
+  | OverviewTabContent
+  | SavingsTabContent
+  | PensionTabContent;
+
+export const isOverviewTabContent = (
+  content: TabBodyContent
+): content is OverviewTabContent => content.type === TabId.OVERVIEW;
+
+export const isSavingsTabContent = (
+  content: TabBodyContent
+): content is SavingsTabContent => content.type === TabId.SAVINGS;
+
+export const isPensionTabContent = (
+  content: TabBodyContent
+): content is PensionTabContent => content.type === TabId.PENSION;
 
 type TabContent = {
   readonly header: TabHeaderContent;
@@ -52,7 +84,11 @@ export const useAppStore = create<AppStoreState>((set) => ({
         description:
           "Welcome to Storebrand. Would you like to start saving with us?",
       },
-      body: {},
+      body: {
+        type: TabId.OVERVIEW,
+        title: "Invest in your future",
+        subtitle: "See our selected funds",
+      },
     },
     [TabId.SAVINGS]: {
       header: {
@@ -60,7 +96,23 @@ export const useAppStore = create<AppStoreState>((set) => ({
         subtitle: "Invest in your future",
         description: null,
       },
-      body: {},
+      body: {
+        type: TabId.SAVINGS,
+        cards: [
+          {
+            title: "Storebrand Fremtid 100 A",
+            subtitle: "New fund",
+          },
+          {
+            title: "Storebrand Verdi A",
+            subtitle: "Up 9.24%",
+          },
+          {
+            title: "Storebrand Indeks - Alle Markeder A",
+            subtitle: "Up 14.13%",
+          },
+        ],
+      },
     },
     [TabId.PENSION]: {
       header: {
@@ -68,7 +120,13 @@ export const useAppStore = create<AppStoreState>((set) => ({
         subtitle: null,
         description: null,
       },
-      body: {},
+      body: {
+        type: TabId.PENSION,
+        title: "Get an overview of your pension",
+        description: `Do you consent to Storebrand Livsforsikring collecting information from Norsk Pensjon about your pension in the National Insurance scheme (Folketrygden) and your private pension policies?
+            
+            The consent is voluntary and is valid until you withdraw it, which you can do at any point from your profile.`,
+      },
     },
   },
   isActionMenuOpen: false,
